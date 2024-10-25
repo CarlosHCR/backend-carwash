48f5edbda16872c27b0c3a6b8213cba29ed992e7
Carlos Henrique <carloshcr5@gmail.com>
2024-10-25 15:42:16 -0300

    refactor: Add new logic for busy times

--- app/carwash/api/v1/views/busy_times.py
+++ app/carwash/api/v1/views/busy_times.py
@@ -7,14 +7,13 @@ API V1: Busy Times Views
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from datetime import datetime, timedelta
-from django.utils import timezone as tz
-import pytz
+from datetime import datetime, timedelta, time
+from django.utils import timezone
+from django.utils.timezone import make_aware
 
-from settings.settings import TIME_ZONE
+
 from app.carwash.models.carwash_service import CarWashService
 
-timezone = pytz.timezone(TIME_ZONE)
 
 ###
 # Viewsets
@@ -32,35 +31,27 @@ class BusyTimesView(APIView):
                     date_selected, '%Y-%m-%d').date()
             except ValueError:
                 return Response({'error': 'Invalid date.'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            start_date = tz.now().date()
+            else:
+                start_date = timezone.now().date()
 
-        if start_date == tz.now().date():
-            start_date += timedelta(days=2)
+            if start_date == timezone.now().date():
+                start_date += timedelta(days=2)
 
-        end_date = start_date + timedelta(days=7)
+            end_date = start_date + timedelta(days=7)
 
-        busy_times = CarWashService.objects.filter(
-            service_date__date__range=[
-                start_date, end_date
+            busy_times = set(CarWashService.objects.filter(
+                service_date__range=[start_date, end_date]
+            ).values_list('service_date', flat=True))
+
+            time_slots = [time(7, 0), time(13, 0)]
+
+            available_times = [
+                make_aware(datetime.combine(date, slot))
+                for date in [start_date + timedelta(days=day) for day in range((end_date - start_date).days + 1)]
+                for slot in time_slots
             ]
-        ).values_list('service_date', flat=True)
 
-        available_times = []
+            available_times = [
+                t for t in available_times if t not in busy_times]
 
-        for day in range((end_date - start_date).days + 1):
-            date = start_date + timedelta(days=day)
-
-            time_0700 = timezone.localize(datetime.combine(
-                date, datetime.strptime('07:00', '%H:%M').time()))
-            time_1300 = timezone.localize(datetime.combine(
-                date, datetime.strptime('13:00', '%H:%M').time()))
-
-            available_times.append(time_0700)
-            available_times.append(time_1300)
-
-        for busy_time in busy_times:
-            if busy_time in available_times:
-                available_times.remove(busy_time)
-
-        return Response(available_times)
+            return Response(available_times, status=status.HTTP_200_OK)
